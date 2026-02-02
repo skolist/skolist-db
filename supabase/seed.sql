ab291778e3e2bcca701db4bc1900ac59426d6170
Skolist <tech@skolist.com>
2026-02-03 03:10:21 +0530

    feat: implement modular seeding structure

---
+++ supabase/seed.sql
@@ -0,0 +1,28 @@
+SET session_replication_role = replica;
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+-- Including the modular files
+\i supabase/seeds/001_boards.sql
+\i supabase/seeds/002_school_classes.sql
+\i supabase/seeds/003_subjects.sql
+\i supabase/seeds/004_chapters.sql
+\i supabase/seeds/005_topics.sql
+\i supabase/seeds/006_concepts.sql
+
+-- RE-ENABLE triggers when finished (Crucial!)
+SET session_replication_role = DEFAULT;
---
+++ supabase/seeds/002_school_classes.sql
@@ -0,0 +1,5 @@
+-- Data for Name: school_classes; Type: TABLE DATA; Schema: public; Owner: postgres
+
+INSERT INTO "public"."school_classes" ("id", "name", "description", "board_id", "position", "created_at", "updated_at") VALUES
+	('e763c901-a3b9-4472-9016-cebfa7a39902', 'Class 10', NULL, '51b7d3cb-b469-4c4e-8c42-70d3c2388fb5', 10, '2025-12-30 19:10:59.829087+00', '2025-12-30 19:10:59.829087+00'),
+	('79eea6a5-591a-4d2b-829f-357f4f2ec236', 'Class 12', 'The Class 12th of the CBSE Board.', '51b7d3cb-b469-4c4e-8c42-70d3c2388fb5', 12, '2026-01-03 15:24:33.073555+00', '2026-01-03 15:24:33.073555+00');
